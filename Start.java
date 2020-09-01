8440a8e9f58b6f270c809253a2af1ae1ac11777e
nabilrt <67606812+nabilrt@users.noreply.github.com>
2020-09-01 13:37:48 +0600

    My Project

--- Start.java
+++ Start.java
@@ -1,22 +1,21 @@
-package Association;
-
+package ArrayList;
+import java.util.ArrayList;
 public class Start {
-
-	public static void main(String[] args) {
-		try{
-		Course ob1=new Course();
-		Course course;
-		Student s1=new Student("Jenee","19-41724-3");
-		s1.insertCourse(ob1);
-		s1.showCourseInformation();
-		}
-		catch(Exception e) {
-			System.out.println(e);
-			System.out.println("Exception Caught!!");
-		}
-		finally {
-			System.out.println("ABCD");
-		}
-	}
-
+public static void main(String[]args) {
+	ArrayList <Integer> ob= new ArrayList<>();
+	int size=ob.size();
+	System.out.println("ArrayList size:"+size);
+	ob.add(10);
+	ob.add(20);
+	int size1=ob.size();
+	System.out.println("ArrayList Elements:"+ob);
+	System.out.println("ArrayList Size:"+size1);
+	ArrayList <Integer> ob1 = new ArrayList<>();
+	ob1.add(40);
+	ob1.add(50);
+	System.out.println("ArrayList Elements 2:"+ob1);
+	ob.addAll(ob1);
+	System.out.println("Now ArrayList:"+ob);
 }
+}
+
